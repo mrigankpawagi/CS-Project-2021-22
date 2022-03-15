ba3e3ebe76a6529f9628eeac90c8213b34975fc5
Jyoti-Shikha <84172711+Jyoti-Shikha@users.noreply.github.com>
2022-03-15 11:07:29 +0530

    Update commons.py

--- commons.py
+++ commons.py
@@ -1,2 +1,26 @@
+import sqlite3 as sql
+from prettytable import PrettyTable
+
 def patientSearch():
-    pass
+    con = sql.connect("patient.db")
+    cur = con.cursor()
+    cur.execute("select * from patients")
+    s = cur.fetchall()
+    print("Please provide the following details:\n")
+    pid = input("Patient ID: ")
+    if len(pid) == 0:
+        print("Incorrect ID. Please try again.")
+        patientSearch()
+    if int(pid) not in [i[0] for i in s]:
+        print("Patient does not exist. Try again")
+        patientSearch()
+    else:
+        cur.execute("select * from patients where id = " + str(pid))
+        res = cur.fetchone()
+        print("\nHere are the search results:\n")
+        pt = PrettyTable()
+        pt.field_names = (["Patient ID", "Name", "Contact"])
+        pt.add_row([res[0], res[2], res[3]])
+        print(pt, "\n")
+
+
