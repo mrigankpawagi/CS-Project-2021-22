ca9c1f9d6ef5d87d826e570ad5c4e9ac04d2e045
Jyoti-Shikha <84172711+Jyoti-Shikha@users.noreply.github.com>
2022-03-15 11:07:33 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -113,26 +113,6 @@ def addSlot():
     
     slotlist()
 
-def closeslot():
-    global slots
-    id = tool.form([("Slot ID",'\d*')])
-    if int(id) not in [S[0] for S in slots]:
-        print("Incorrect ID. Please try again.")
-        closeslot()
-    else:
-        if slots[[S[0] for S in slots].index(int(id))][15] == "Closed": 
-            print("Closed slots cannot be closed again.")
-        else:
-            name = input("Enter prescription name: ")
-            tool.updateQuery("admin", "slots", {"presfilename":name}, 'id = "{}"'.format(id))
-            c = input("How do you want to upload the prescription?\n1. Text\n2. File\n")
-            if c == "1":
-                pres = input("Enter closing prescription: ")
-                tool.updateQuery("admin", "slots", {"prestext":pres}, 'id = "{}"'.format(id)) 
-            elif c == "2":
-                pres = input("Enter file path: ")
-                tool.insertblob(id, pres)
-
 def gethistory():
     global slots
     id = input("Patient ID: ")
